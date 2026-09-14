255842dc5c055e7cc6e0278d445f89d9b953e4aa
dibyajit1234 <dibyajitnandi7205@gmail.com>
2026-09-14 23:46:50 +0530

    updated

--- backend/fraud-dection/src/main/java/com/banking/frauddection/service/FraudDetectionService.java
+++ backend/fraud-dection/src/main/java/com/banking/frauddection/service/FraudDetectionService.java
@@ -4,12 +4,16 @@ import com.banking.frauddection.client.AccountServiceClient;
 import com.banking.frauddection.model.FraudCheckResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -18,9 +22,20 @@ public class FraudDetectionService {
 
     private final KafkaTemplate<String,Object> kafkaTemplate;
     private final AccountServiceClient accountServiceClient;
+    private final RedisTemplate<String,String> redisTemplate;
+
+    @Value("${fraud.max-transactions-per-minute}")
+    private int maxTransactionsPerMinute;
+
+    @Value("${fraud.suspicious-amount-multiplier}")
+    private double suspiciousAmountMultiplier;
+
+    @Value(("${fraud.max-Balance-Percentage}"))
+    private double maxBalancePercentage;
 
     private static final String VERIFICATION_REQUIRED_TOPIC = "verification.required";
     private static final String FRAUD_CHECK_CLEAN_RESULT = "fraud.check.clean";
+
 
     public void checkTransaction(Map<String, Object> payload) {
         String transactionId = (String) payload.get("transactionId");
@@ -32,7 +47,7 @@ public class FraudDetectionService {
 
         log.info("Checking transaction:{}, account number: {}, amount : {}, balance : {},",
                 transactionId,accountNumber,amount,senderBalance);
-        FraudCheckResult result = performFraudCheck(accountNumber,amount,senderBalance);
+        FraudCheckResult result = performFraudCheck(accountNumber,new BigDecimal(amount),senderBalance);
 
         if(result.isFraud()){
             log.info("Suspicious activity detected, account: {}, reason :{}, requesting OTP verification"
@@ -63,10 +78,10 @@ public class FraudDetectionService {
             , BigDecimal senderBalance) {
 
         //pattern 1: velocity check
-        if(isVelocityExceeded)return new FraudCheckResult(true,"To many transactions in 60 seconds - velocity limit exceeded");
+        if(isVelocityExceeded(accountNumber))return new FraudCheckResult(true,"To many transactions in 60 seconds - velocity limit exceeded");
 
         //pattern 2 : amount check
-        if(isAccountSuspicious(accountNumber,amount)) return new FraudCheckResult(true,"Unusual transaction amount - exceeds yous average")
+        if(isAccountSuspicious(accountNumber,amount)) return new FraudCheckResult(true,"Unusual transaction amount - exceeds yous average");
 
         //pattern 3: Balance check
         if(senderBalance.compareTo(BigDecimal.ZERO)>0 && isBalanceCheckFailed(senderBalance,amount))
@@ -75,4 +90,43 @@ public class FraudDetectionService {
         return new FraudCheckResult(false,null);
 
     }
+
+    private boolean isVelocityExceeded(String accountNumber){
+        String key = "fraud:velocity"+accountNumber;
+        Long count = redisTemplate.opsForValue().increment(key);
+
+        if(count != null && count==1) redisTemplate.expire(key,60, TimeUnit.SECONDS);
+        log.info("Velocity check-> accountNumber: {}, count:{}",accountNumber,count);
+        return count!=null && count > maxTransactionsPerMinute;
+    }
+
+    private boolean isAccountSuspicious(String accountNumber, BigDecimal amount){
+        String avgKey = "fraud:avg_amount" + accountNumber;
+        String avgStr = redisTemplate.opsForValue().get(avgKey);
+        if(avgStr==null){
+            redisTemplate.opsForValue().set(avgKey,amount.toString());
+            return false;
+        }
+        BigDecimal avgAmount = new BigDecimal(avgStr);
+        BigDecimal threshold = avgAmount.multiply(BigDecimal.valueOf(suspiciousAmountMultiplier));
+        //update running average
+        BigDecimal newAvg = avgAmount.add(amount)
+                .divide(BigDecimal.valueOf(2),2, RoundingMode.HALF_UP);
+        redisTemplate.opsForValue().set(avgKey,newAvg.toString());
+        log.info("amount check - amount {} threshold {} suspicious {}",
+                amount,threshold,amount.compareTo(threshold)>0);
+        return amount.compareTo(threshold)>0;
+
+    }
+
+    private boolean isBalanceCheckFailed(BigDecimal senderBalance, BigDecimal amount){
+        BigDecimal maxAllowed = senderBalance.multiply(
+                BigDecimal.valueOf(maxBalancePercentage)
+        );
+        log.info("Balance check - amount {} maxAllowed {} suspicious {}"
+        ,amount,maxAllowed,amount.compareTo(maxAllowed)>0);
+        return amount.compareTo(maxAllowed)>0;
+    }
+
+
 }
--- backend/transaction-service/src/main/java/com/banking/transactionservice/service/TransactionEventConsumer.java
+++ backend/transaction-service/src/main/java/com/banking/transactionservice/service/TransactionEventConsumer.java
@@ -1,4 +1,76 @@
 package com.banking.transactionservice.service;
 
+import com.banking.transactionservice.entity.Transaction;
+import com.banking.transactionservice.entity.TransactionStatus;
+import com.banking.transactionservice.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class TransactionEventConsumer {
+
+    private final TransactionRepository transactionRepository;
+    private final RedisTemplate<String,String> redisTemplate;
+    private static final long OTP_EXPIRY_MINUTES  =5;
+
+    private final KafkaTemplate<String,Object> kafkaTemplate;
+
+    private static final String TRANSACTION_OTP_GENERATED_TOPIC = "transaction.otp.generated";
+    /*
+    * Consume verification.required
+    * Generate otp and ask to verify
+    * */
+    public void consumerVerificationRequired(
+            @Payload Map<String,Object> payload){
+        try {
+            String transactionId = (String)payload.get("transactionId");
+            String reason = (String) payload.get("reason");
+            String accountNumber = (String)payload.get("accountNumber");
+
+            log.info("Verification required - transaction {} reason {}",transactionId,reason);
+
+            Transaction transaction = transactionRepository.findById(transactionId)
+                    .orElseThrow(()-> new RuntimeException("Transaction not fount" + transactionId));
+            if (transaction.getStatus()!= TransactionStatus.PROCESSING){
+                log.warn("Transaction {} not processing skipping",transactionId);
+                return ;
+            }
+            //Generate 6 digit otp
+            String otp = String.format("%06d",(int)(Math.random() * 900000)+100000);
+
+            //store otp in redis - expires in 5 minutes
+            String otpKey = "verification:otp"+transactionId;
+            redisTemplate.opsForValue().set(otpKey,otp,OTP_EXPIRY_MINUTES, TimeUnit.MINUTES);
+            //update status
+            transaction.setStatus(TransactionStatus.PENDING_VERIFICATION);
+            transactionRepository.save(transaction);
+            log.info("otp generated for transaction {} expires in {} minutes"
+                    ,transactionId
+                    ,OTP_EXPIRY_MINUTES);
+            //notify user
+            Map<String,Object> otpEvent = new HashMap<>();
+            otpEvent.put("transactionId",transaction);
+            otpEvent.put("accountNumber",accountNumber);
+            otpEvent.put("reason",reason);
+            otpEvent.put("otp",otp);
+            otpEvent.put("amount",payload.get("amount"));
+
+            kafkaTemplate.send(TRANSACTION_OTP_GENERATED_TOPIC,otpEvent);
+
+
+        } catch (Exception e) {
+            log.info("Error handling verification required {}",e.getMessage());
+        }
+
+    }
 }
